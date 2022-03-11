691a5c6db0517a41812e6dbf38fd53449991f91e
Mario Lopez <ml09231999>
2022-03-10 19:02:30 -0500

    Se edito la conexion DB

--- modelo/Conexion.php
+++ modelo/Conexion.php
@@ -1,12 +1,12 @@
 <?php
 class Conexion
 {
-     private $servidor = "localhost";
-     private $db = "farmacia";
+     private $servidor = "ftp.dimerlyn.com";
+     private $db = "dimerlyn_bdmerlyn";
      private $puerto = 3306;
      private $charset = "utf8";
-     private $usuario = "root";
-     private $contrasena = "";
+     private $usuario = "dimerlyn_dimerlyn";
+     private $contrasena = "Maau09231999";
      public $pdo = null;
      private $atributos = [PDO::ATTR_CASE => PDO::CASE_LOWER, PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION, PDO::ATTR_ORACLE_NULLS => PDO::NULL_EMPTY_STRING, PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_OBJ];
      //PDO:FETCH_PBJ -> retorna obejtos en la consulta
